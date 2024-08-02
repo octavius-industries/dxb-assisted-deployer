d193e1c4f70c80c3f2fba3e6a2883617e9d8a801
mrbrique <locus.chan.dev@gmail.com>
2024-08-02 21:21:18 +0100

    spring initialized

---
+++ src/main/java/com/ock/assisteddeploy/dxb/DxbAssistedDeployer.java
@@ -0,0 +1,13 @@
+package com.ock.assisteddeploy.dxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxbAssistedDeployer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxbAssistedDeployer.class, args);
+    }
+
+}
---
+++ src/test/java/com/ock/assisteddeploy/dxb/DxbAssistedDeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ock.assisteddeploy.dxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxbAssistedDeployerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
