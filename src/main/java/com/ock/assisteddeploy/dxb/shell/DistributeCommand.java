bc7227d3ec3e7ba0e056f743bfdb3e612c3f94bb
mrbrique <locus.chan.dev@gmail.com>
2024-08-06 17:13:40 +0100

    Create DistributeCommand.java

---
+++ src/main/java/com/ock/assisteddeploy/dxb/shell/DistributeCommand.java
@@ -0,0 +1,27 @@
+package com.ock.assisteddeploy.dxb.shell;
+
+import com.ock.assisteddeploy.dxb.distribute.ArtifactDistributor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class DistributeCommand {
+
+    private static final Logger logger = LoggerFactory.getLogger(DistributeCommand.class);
+
+    @Autowired
+    private ArtifactDistributor distributor;
+
+    @ShellMethod(
+            value = "Distribute the artifact vault to the specified target locations or servers.",
+            key = "put"
+    )
+    public void distribute() {
+        logger.info("Distribute start.");
+        distributor.distribute();
+        logger.info("Distribute complete.");
+    }
+}
