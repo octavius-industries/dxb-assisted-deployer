e7a610a2753a6b20a33a37d474dd175df59dc484
mrbrique <locus.chan.dev@gmail.com>
2024-08-09 16:35:02 +0100

    implement WorkflowSortingStrategy.compare to refer WorkflowOrderVisitor value

--- src/main/java/com/ock/assisteddeploy/dxb/shell/WorkflowSortingStrategy.java
+++ src/main/java/com/ock/assisteddeploy/dxb/shell/WorkflowSortingStrategy.java
@@ -4,7 +4,13 @@ import java.util.Comparator;
 
 public class WorkflowSortingStrategy implements Comparator<Command> {
     @Override
-    public int compare(Command o1, Command o2) {
-        return 0;
+    public int compare(Command cmd1, Command cmd2) {
+        WorkflowOrderVisitor orderVisitor1 = new WorkflowOrderVisitor();
+        WorkflowOrderVisitor orderVisitor2 = new WorkflowOrderVisitor();
+
+        cmd1.accept(orderVisitor1);
+        cmd2.accept(orderVisitor2);
+
+        return orderVisitor1.getOrder() - orderVisitor2.getOrder();
     }
 }
